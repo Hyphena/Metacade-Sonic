bd480735989d1711c415baebf36ba1ef0544af6d
Hyphena <serenelunation@gmail.com>
2019-06-21 20:00:02 -0400

    Implemented Running

--- main.js
+++ main.js
@@ -22,15 +22,15 @@ function think(time, dt)
 
 function onKeyPressed(key)
 {
-    if (key == 4) { player.leftDown = -1; }
-    if (key == 7) { player.rightDown = 1; }
+    if (key == 4) { player.leftDown = true; player.lastDown = false; }
+    if (key == 7) { player.rightDown = true; player.lastDown = true; }
 }
 
 
 function onKeyReleased(key)
 {
-    if (key == 4) { player.leftDown = 0; }
-    if (key == 7) { player.rightDown = 0; }
+    if (key == 4) { player.leftDown = false; }
+    if (key == 7) { player.rightDown = false; }
 }
 
 
@@ -40,10 +40,19 @@ function player(x, y)
     this.y = y || 0;
     this.w = 12;
     this.h = 24;
-    this.speed = 5;
 
-    this.leftDown = 0;
-    this.rightDown = 0;
+    this.xsp = 0;
+    this.ysp = 0;
+    this.gsp = 0;
+
+    this.acc = 0.046875;
+    this.dec = 0.5;
+    this.frc = 0.046875;
+    this.top = 6;
+
+    this.leftDown = false;
+    this.rightDown = false;
+    this.lastDown = true; // false = left; true = right
 
 
     this.draw = function()
@@ -54,6 +63,25 @@ function player(x, y)
 
     this.think = function()
     {
-        this.x += this.speed * (this.leftDown + this.rightDown);
+        this.running();
+    }
+
+
+    this.running = function()
+    {
+        if (this.leftDown)
+        {
+            if (this.gsp > 0) { this.gsp -= this.dec; }
+            else if (this.gsp > -this.top) { this.gsp -= this.acc; }
+        }
+        else if (this.rightDown)
+        {
+            if (this.gsp < 0) { this.gsp += this.dec; }
+            else if (this.gsp < this.top) { this.gsp += this.acc; }
+        }
+        else { this.gsp -= Math.min(Math.abs(this.gsp), this.frc) * Math.sign(this.gsp); }
+
+        this.xsp = this.gsp * Math.cos(0);
+        this.x += this.xsp;
     }
 }
